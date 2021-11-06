88db73f26f843c9b837cc9aed77f5dcb8ec634d2
Darylabrador <abrador.daryl@gmail.com>
2021-11-06 22:35:56 +0400

    Adding : splashscreen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/splashscreen.png"),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+    );
+  }
+}
